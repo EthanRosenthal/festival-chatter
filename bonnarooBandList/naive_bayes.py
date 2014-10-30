49a6f20a3c03fc996961b8e47ebfc9252beb07ae
Ethan Rosenthal <ethanrosenthal@gmail.com>
2014-10-30 01:17:40 -0400

    changed from pandas to X, y

--- bonnarooBandList/naive_bayes.py
+++ bonnarooBandList/naive_bayes.py
@@ -1,11 +1,54 @@
 import pandas as pd
 import numpy as np
+import sklearn.cross_validation as cross_val
 
-"""
-NEED TO RE-DO FOR NON-PANDAS USAGE... OR JUST FIGURE OUT THE TRAIN-TEST SPLIT BETTER
-"""
+def train_then_test(df, band_list):
 
-def make_train_test_split(df, band_list):
+    X, y = pandas_to_Xy(df, band_list)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    prior, conditional_prob, min_prob = \
+                train_probabilities(X_train, y_train, band_list)
+
+    vclassify_map = np.vectorize(lambda x:
+                    classify_map(x,
+                                band_list,
+                                min_prob,
+                                prior,
+                                conditional_prob,
+                                )
+                                )
+    y_train_pred = vclassify_map(X_train)
+    print 'Training Data: \n'
+    calculate_accuracy(y_train_pred, y_train)
+
+    y_test_pred = vclassify_map(X_test)
+    print 'Test Data: \n'
+    calculate_accuracy(y_test_pred, y_test)
+
+    print 'Training Error by Artist: \n'
+    for band in band_list:
+        print '%s' % band
+        calculate_accuracy(y_train_pred[ y_train==band ],
+                    y_train[y_train==band]
+                    )
+    # return conditional_prob
+
+
+def calculate_accuracy(y_pred, y_true):
+    accuracy = float(np.sum(y_pred==y_true))/len(y_pred)
+
+    print 'Accuracy: %.4f \n' % accuracy
+
+def train_test_split(X, y):
+    X_train, X_test, y_train, y_test \
+             = cross_val.train_test_split(X,
+    r                                      y,
+                                          test_size=0.2,
+                                          random_state=33)
+    return X_train, X_test, y_train, y_test
+
+def pandas_to_Xy(df, band_list):
     X = np.empty(0)
     y = np.empty(0)
     for band in band_list:
@@ -18,15 +61,14 @@ def make_train_test_split(df, band_list):
     return X, y
 
 
+def train_probabilities(X, y, band_list):
 
-def train_probabilities(df, band_list):
+    prior = calculate_prior_probability(X, y, band_list)
 
-    prior = calculate_prior_probability(df, band_list)
-
-    class_bag = make_class_documents(df, band_list)
+    class_bag = make_class_documents(X, y, band_list)
 
     conditional_prob, min_prob = \
-        calculate_conditional_probability(df, band_list, class_bag)
+        calculate_conditional_probability(band_list, class_bag)
     return prior, conditional_prob, min_prob
 
 def classify_map(text, band_list, min_prob, prior, conditional_prob):
@@ -44,22 +86,21 @@ def classify_map(text, band_list, min_prob, prior, conditional_prob):
     return band_class
 
 
-def calculate_prior_probability(df, band_list):
+def calculate_prior_probability(X, y, band_list):
     # Calculate prior probability
     prior = {}
-    total_docs = float(0)
-    for band in band_list:
-        total_docs += df[band].value_counts()[True]
+    total_docs = float(len(y))
 
     for band in band_list:
-        prior[band] = float(df[band].value_counts()[True])/total_docs
+        prior[band] = float(len( y[ y==band ] ))/total_docs
 
     return prior
 
-def calculate_conditional_probability(df, band_list, class_bag):
+def calculate_conditional_probability(band_list, class_bag):
     # Calculate conditional probability
     conditional_prob = {}
     min_prob = {}
+    alpha = 1.
     for band in band_list:
         tmp_prob = {}
         # Get total number of words for a given band
@@ -69,29 +110,33 @@ def calculate_conditional_probability(df, band_list, class_bag):
         vocab_size = float(len(class_bag.keys()))
         for k, v in class_bag[band].iteritems():
             v = float(v)
-            tmp_prob[k] = (v + 1.)/(total_class_word_count +
-                            vocab_size)
+            tmp_prob[k] = (v + alpha*1.)/(total_class_word_count +
+                            alpha*vocab_size)
         conditional_prob[band] = tmp_prob
-        min_prob[band] = 1./(total_class_word_count + vocab_size)
+        min_prob[band] = alpha*1./(total_class_word_count + alpha*vocab_size)
     return conditional_prob, min_prob
 
 
-def make_class_documents(df, band_list):
+def make_class_documents(X, y, band_list):
     """
     Create a bag of words and associated word counts for each band in band_list. The output is a dictionary with key for each band and the value is a dictionary containing words as keys and word counts as values for every tweet that mentioned that band.
     """
     class_bag = {}
     for band in band_list:
-        class_bag[band] = word_count(df[df[band] == True])
+        class_bag[band] = word_count(X[y == band])
     return class_bag
 
-def word_count(df):
-    # Map
-    mapout = df['tokens'].apply(lambda x : mapper(x))
+def word_count(X):
+    # Vectorize map function and apply to X
+    vmapper = np.vectorize(lambda x: mapper(x))
+    mapout = vmapper(X)
+
     global big_bag
     big_bag = {}
-    # Reduce
-    mapout.apply(lambda x : reducer(x, big_bag), big_bag)
+
+    # Vectorize reduce function and apply to mapout
+    vreducer = np.vectorize(lambda x: reducer(x, big_bag))
+    vreducer(mapout)
 
     return big_bag
 

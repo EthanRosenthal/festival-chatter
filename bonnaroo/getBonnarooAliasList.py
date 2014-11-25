246bb465292ec063539d909c75328c7e0bcd21d2
Ethan Rosenthal <ethanrosenthal@gmail.com>
2014-11-25 00:55:12 -0500

    cleaned up

--- bonnaroo/getBonnarooAliasList.py
+++ bonnaroo/getBonnarooAliasList.py
@@ -1,14 +1,16 @@
 """
-Created on Mon June 30 2014
+Use the musicbrainz API to get known aliases for a queried band name.
 
-@author: Ethan Rosenthal
+INPUTS
+bonarooBandList.txt = Read in automatically below. Outputted from
+    getBonnarooBandList.py Each band in file is used as a query to
+    the musicbrainz API
 
-This routine uses the musicbrainz API to get known aliases for a queried band name.
-The results of the API GET request are returned as a dictionary.
-The code below normalizes the dictionary results by removing punctuation and ampersands
-and saves the results as a JSON file.
-The JSON output file uses the original band name that was read in by the input band list
-as the dict key and a list of aliases as the dict value
+OUTPUTS
+bonnarooAliasList.josn = Outputted automatically below. Consists of keys
+    corresponding to each band in input file. Values are a list of aliases
+    returned from the API GET request. Alias lists are tokenized and
+    stripped of stopwords.
 """
 
 import musicbrainzngs as mbrainz
@@ -21,16 +23,16 @@ from nltk.corpus import stopwords
 
 mbrainz.auth('rosentep','testing') # Omitted for github
 mbrainz.set_useragent('Btest/0.0','x@gmail.com') # Omitted for github
-
 # To be used for removing punctuation
 regex = re.compile('[%s]' % re.escape(string.punctuation))
+
 
 ################################
 def clean_aliases(alias, regex):
     """
-    This routine converts each alias to lowercase, removes
-    punctuation, and removes stop words. The output is a
-    list containing the remaining words.
+    Convert each alias to lowercase, remove punctuation,
+    and remove stop words. The output is a list containing
+    the remaining words.
     """
     alias = alias.lower().replace(' &amp;', '') # Remove ampersands
     alias = regex.sub('', alias) # Remove punctuation
@@ -38,9 +40,12 @@ def clean_aliases(alias, regex):
     return alias_words
 ################################
 
+
+# MAIN CODE
 with open('bonnarooBandList.txt','r') as fin:
     with open('bonnarooAliasList.json','w') as fout:
-    	aliasDict={} # Initialize alias dictionary
+
+    	aliasDict={}
         for band in fin:
             band = band.rstrip('\n')
             band_query = band.lower().replace(' &amp;', '') # Remove ampersands
@@ -50,13 +55,18 @@ with open('bonnarooBandList.txt','r') as fin:
             band_query = clean_aliases(band_query, regex) # Remove stopwords
             aliasList = [band_query] # Initialize with stripped version of name
                                      # listed on Bonnaroo website
+
+            # result is large JSON file. Extract alias list
             try:
             	for alias in result['artist-list'][0]['alias-list']:
                     alias = clean_aliases(alias['alias'], regex)
                     aliasList.append(alias) # Build alias List
-            except: # Some artists do not return aliases
-            	pass # So don't do anything!
+            except:
+                # Some artists do not return aliases
+            	pass
             aliasDict[band] = aliasList
+
     	json.dump(aliasDict,fout)
+
     fout.close()
 fin.close()
